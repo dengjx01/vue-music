edb17e4cc2f13b0dc1910164b6e6a25744089eac
dengjunxiang <583214339@qq.com>
2018-11-24 15:01:45 +0800

    播放器进度条

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -20,7 +20,8 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?fromtag=46`
+    // url: `http://dl.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?fromtag=38`
+    url: `http://dl.stream.qqmusic.qq.com/M500${musicData.songmid}.mp3?guid=2095717240&vkey=74819C895A712D3B6B64F65F1FE19640D57ABCB02DA1E9C249B3C58B87C58C7FA9753255F1A1C4267E11AE943A702B1BF2CD689B375B1EDD&uin=0&fromtag=53`
   })
 }
 
